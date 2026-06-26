08bc2dba01360950ee5e67bde1bc67c664bec434
truongtuandung2001 <truongtuandung2001@gmail.com>
2026-06-26 10:41:18 +0700

    Fix bug parallax location banner

--- client/js/app.js
+++ client/js/app.js
@@ -1,8 +1,14 @@
+//
+const banner = document.querySelector('.banner');
 const bannerImg = document.querySelector('.banner-img');
+const categoryImg = document.querySelector('.category-img');
 
+//
 window.addEventListener('scroll', () => {
-  const scrollY = window.scrollY;
+  // const scrollY = window.scrollY; //lấy vị trí của window scroll từ đầu
+  const rectBanner = banner.getBoundingClientRect();//lấy vị trí của banner để scroll lun 
 
-  bannerImg.style.transform =
-    `translateY(${scrollY * .4}px)`;
+  bannerImg.style.transform = `translateY(${-rectBanner.top * .2}px)`;
+
 });
+
