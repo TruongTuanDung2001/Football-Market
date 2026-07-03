cd6fdd6a3664009ee55e14f8aafca72209cd1281
truongtuandung2001 <truongtuandung2001@gmail.com>
2026-07-03 13:33:24 +0700

    First post api product in dashboard

--- client/js/pages/dashboard/products.js
+++ client/js/pages/dashboard/products.js
@@ -15,11 +15,73 @@ export function initProduct() {
     closeModal.addEventListener("click", () => {
         modal.classList.remove("show");
     });
+
+    postProductApi();
+}
+
+export function postProductApi() {
+    const btnSubmit = document.querySelector('#btn-submit');
+
+    btnSubmit.addEventListener('click', async function (e) {
+        let productName = document.querySelector('#productName').value;
+        let category = document.querySelector('#category').value;
+        let quantity = document.querySelector('#quantity').value;
+        let price = document.querySelector('#price').value;
+        let sale = document.querySelector('#sale').value;
+        let image = document.querySelector('#image');
+        let description = document.querySelector('#description').value;
+
+        e.preventDefault();
+
+
+        const file = image.files[0];
+        const imagePath = `/client/assets/images/${file.name}`;
+        console.log(productName, category, quantity, price, sale, image.value, file.name, description);
+        console.log(imagePath);//đường dẫn tệp file trong project để lấy file image
+        //kiểm tra chọn ảnh chưa
+
+        if (!file) {
+            alert("Vui lòng chọn ảnh!");
+            return;
+        }
+        //Post api nè
+        const newProduct = {
+            name: productName,
+            price: Number(price),
+            sale: Number(sale),
+            quantity: Number(sale),
+            category: category,
+            image: imagePath,
+            description: description,
+            createdAt: new Date().toISOString(),
+            updatedAt: new Date().toISOString()
+        }
+        try {
+            let response = await fetch(`http://localhost:3000/products`, {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(newProduct)
+            }
+            );
+            if (response.ok) {
+                let data = await response.json();
+                console.log(data);
+                console.log('success');
+            } else {
+                console.log('false');
+
+            }
+        } catch (error) {
+            throw new Error(error);
+        }
+    });
 }
 
 //handle link image front-end
 /*
-    const file = imageInput.files[0];
+const file = imageInput.files[0];
 
 const imagePath = `/client/assets/images/${file.name}`;
 
