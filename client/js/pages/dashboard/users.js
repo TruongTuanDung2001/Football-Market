e1f7431108320e7916b451292e1635f2c8c0e61f
truongtuandung2001 <truongtuandung2001@gmail.com>
2026-07-10 12:14:19 +0700

    Render api products in html

--- client/js/pages/dashboard/users.js
+++ client/js/pages/dashboard/users.js
@@ -2,7 +2,53 @@ import { getApi } from "../../api/api.js";
 
 const apiUsers = await getApi('users')
 
-export async function getAllUsers(){
+export async function getAllUsers() {
     console.log(apiUsers);
+    renderListUser();
+}
+
+async function renderListUser() {
+    let usersList = document.getElementById('users-list');
+    let html = '';
+    if (await apiUsers) {
+        let users = apiUsers.filter(user => user.role == "user");
+        users.forEach(user => {
+            html += `
+            <tr>
+              <td>${user.fullName}</td>
+
+              <td>${user.email}</td>
+
+              <td>
+                <span class="role admin"> ${user.role} </span>
+              </td>
+
+              <td>${user.createdAt}</td>
+
+              <td>
+                <span class="status active"> ${user.status}</span>
+              </td>
+
+              <td>
+                <div class="action">
+
+                  <button class="edit">
+                    <i class="fa-solid fa-pen"></i>
+                  </button>
+
+                  <button class="ban">
+                    <i class="fa-solid fa-user-slash"></i>
+                  </button>
+
+                  <button class="delete">
+                    <i class="fa-solid fa-trash"></i>
+                  </button>
+                </div>
+              </td>
+            </tr>
+            `
+        });
+    }
+    usersList.innerHTML = html;
     
 }
